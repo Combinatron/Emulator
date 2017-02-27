e98714b5c399031e4aacd3b2ca3093390e6cf223
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-02-26 22:00:38 -0500

    Add lens

--- Combinatron-Emulator.nix
+++ Combinatron-Emulator.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv, vector, buildTools ? [] }:
+{ mkDerivation, base, stdenv, vector, lens, buildTools ? [] }:
 mkDerivation {
   pname = "Combinatron-Emulator";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base vector ];
+  executableHaskellDepends = [ base vector lens ];
   homepage = "http://github.com/Combinatron/Emulator";
   description = "A software implementation of the combinatron virtual machine";
   license = stdenv.lib.licenses.unfree;
