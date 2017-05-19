a14a419c5fdf8214af6f0b69c70fd10accca05c5
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-05-18 21:02:52 -0400

    Add command parsing framework

--- Combinatron-Emulator.nix
+++ Combinatron-Emulator.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv, vector, lens, QuickCheck, hspec, binary-strict, bytestring, buildTools ? [] }:
+{ mkDerivation, base, stdenv, vector, lens, QuickCheck, hspec, binary-strict, bytestring, optparse-simple, buildTools ? [] }:
 mkDerivation {
   pname = "Combinatron-Emulator";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base vector lens QuickCheck hspec binary-strict bytestring];
+  executableHaskellDepends = [ base vector lens QuickCheck hspec binary-strict bytestring optparse-simple];
   homepage = "http://github.com/Combinatron/Emulator";
   description = "A software implementation of the combinatron virtual machine";
   license = stdenv.lib.licenses.unfree;
--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,24 @@
 module Main where
 
 import Combinatron (run)
-import System.Environment (getArgs)
 import Combinatron.Types (initialize, printMachine)
 import Combinatron.Loader
 import qualified Data.ByteString.Lazy as B
+import Options.Applicative.Simple (simpleOptions, addCommand, argument, str, metavar, pure)
+
+commandLine =
+    simpleOptions
+        "prototype"
+        "Combinatron Emulator"
+        "An emulator for the Combinatron processor"
+        (pure ()) $ do
+            addCommand
+                "run"
+                "Run a program and print the final state"
+                (\ s -> (run, s))
+                (argument str (metavar "FILE"))
 
 main = do
-    input <- head <$> getArgs
+    (_, (runCmd, input)) <- commandLine
     prog <- loadFile <$> B.readFile input
-    printMachine $ run (initialize prog)
+    printMachine $ runCmd (initialize prog)
