534ee383acef2f668f8ba14f3375f16a4c82bd10
Daniel Wilson-Thomas <daniel@mojotech.com>
2018-04-22 19:48:55 -0400

    Extract runner from run commands

--- src/Combinatron.hs
+++ src/Combinatron.hs
@@ -4,23 +4,28 @@ module Combinatron (
   runN
 ) where
 
-import Combinatron.Types (printMachine)
+import Combinatron.Types (printMachine, Machine)
 import System.IO.Unsafe (unsafePerformIO)
 import Combinatron.Reducer
 import Combinatron.Sparker
 import Combinatron.Operations (updateRoot)
+import Control.Monad ((<=<))
 
-run m = case (sparkRandom . r  . r $ m) of
+run m = case (runner m) of
     (Right m) -> run m
     (Left m) -> m
     where
+        r :: Machine -> Either Machine Machine
         r = step . updateRoot
+        runner = fmap sparkRandom . r <=< r
 
-runDebug m = case (sparkRandom . r . r $ m) of
+runDebug m = case (runner m) of
     (Right m) -> runDebug m
     (Left m) -> m
     where
+        r :: Machine -> Either Machine Machine
         r = step . updateRoot . unsafePerformIO . printMachine
+        runner = fmap sparkRandom . r <=< r
 
 runN 0 m = m
 runN n m = case step m of
