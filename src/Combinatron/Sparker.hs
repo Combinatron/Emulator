1ee44e0d0b5ea40f725fefcf63629fa731d3028c
Daniel Wilson-Thomas <daniel@mojotech.com>
2018-04-25 21:36:14 -0400

    Don't spark random tasks on partially evaluated work

--- src/Combinatron/Sparker.hs
+++ src/Combinatron/Sparker.hs
@@ -20,15 +20,22 @@ sparkRandom m = sparkTask (newPointer $ rIndex) m
         -- Start at the second index, and end at the last.
         rIndex = unsafePerformIO $ randomRIO (2, (view (sentenceIndex.to V.length) m))
 
-ifNotFull :: (Machine -> Machine) -> Machine -> Machine
-ifNotFull f m
+canSparkTask :: Pointer -> (Machine -> Machine) -> Machine -> Machine
+canSparkTask p f m
+    | isMWord p m = m -- can't spark a task on a partially evaluated expression
     | isRootsFull m = m
     | otherwise = f m
+
+isMWord :: Pointer -> Machine -> Bool
+isMWord p m = isM w
+    where
+        s = sentenceAt p (view sentenceIndex m)
+        w = view priWord s
 
 -- To spark a task, manipulate index to include sparked word
 -- Add location of new, original sentence to task roots.
 sparkTask :: Pointer -> Machine -> Machine
-sparkTask index m = ifNotFull (uncurry addRoot . sparkWord index) m
+sparkTask index m = canSparkTask index (uncurry addRoot . sparkWord index) m
 
 -- Move the sentence at the location
 -- Write a single Sparked word in the index location, with the location of the moved sentence
