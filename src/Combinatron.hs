dfe2a4a439ab5ba5d94c613b0bb9a97b6fa7c1ae
Daniel Wilson-Thomas <daniel@mojotech.com>
2018-04-25 20:18:21 -0400

    More refactors to combinatron entry
    Debug prints each step now

--- src/Combinatron.hs
+++ src/Combinatron.hs
@@ -2,9 +2,10 @@ module Combinatron (
   run,
   runDebug,
   runN,
-  runner
+  cycle
 ) where
 
+import Prelude hiding (cycle)
 import Combinatron.Types (printMachine, Machine)
 import Combinatron.Reducer
 import Combinatron.Sparker
@@ -14,26 +15,26 @@ import Control.Monad ((=<<))
 
 infinity = 1/0
 
-run = runN infinity
+run = runN megacycle infinity
 
 runDebug m = do
     prompt
-    let m' = runN 1 m
+    let m' = runN cycle 1 m
     printMachine m'
-    runDebug m'
+    prompt
+    let m'' = runN (fmap sparkRandom . cycle) 1 m'
+    printMachine m''
+    runDebug m''
 
 prompt = getLine
 
-runner m r = fmap sparker $ stepTwo =<< stepOne
-    where
-        stepOne = fmap collect (r m)
-        stepTwo m' = fmap collect (r m')
-        sparker m'' = sparkRandom m''
+megacycle m = fmap sparkRandom $ cycle =<< cycle m
 
-runN 0 m = m
-runN n m = case runner m r of
-    (Right m') -> runN (n - 1) m'
+runN :: (Num a, Eq a, Enum a) => (Machine -> Either Machine Machine) -> a -> Machine -> Machine
+runN _ 0 m = m
+runN f n m = case f m of
+    (Right m') -> runN f (pred n) m'
     (Left m) -> m
-    where
-        r :: Machine -> Either Machine Machine
-        r = step . updateRoot
+
+cycle :: Machine -> Either Machine Machine
+cycle = fmap collect . step . updateRoot
